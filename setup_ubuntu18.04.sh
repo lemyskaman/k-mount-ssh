fc62d6a658282571a9ff47e5d9deef222d341a66
lemyskaman <lemyskmaan@gmail.com>
2020-02-29 10:45:18 -0400

    seting some defaults

--- setup_ubuntu18.04.sh
+++ setup_ubuntu18.04.sh
@@ -7,14 +7,7 @@
 
 echo "giving executable permisions on kmountssh"
 sudo chmod +x kmountssh
-echo "atemping to creating kmountssh softlink on /usr/bin"
 
-FILE="/usr/bin/kmountssh"
-if [ -L "$FILE" ]; then
-    echo "the softlink already exists"
-else 
-	sudo ln -s kmountssh $FILE
-fi
 echo "atemping to create the main folder were to mount remote directories at ~/kmountssh_computers"
 FILE="/home/kaman/kmountssh_computers"
 if [ -d "$FILE" ]; then
