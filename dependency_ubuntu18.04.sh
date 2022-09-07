f3eef75511554314f9b44bbba38c9525e83f5c1b
lemys <lemyskaman@gmail.com>
2022-09-06 20:08:55 -0400

    adding compatibility with ubuntu22.04

--- dependency_ubuntu18.04.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-# author: lemys lopez
-# mail: lemyskaman@gmail.com
-# description: install all ubuntu 18.04 dependcyes and  kmountssh script to work on your distro 
-
-clear
-echo "installing dependcies"
-sudo apt-get install ssh fuse sshfs zenity 
--- setup_ubuntu22.04.sh
+++ setup_ubuntu22.04.sh
@@ -5,7 +5,7 @@
 
 clear
 echo "installing dependcies"
-sudo apt-get install ssh fuse sshfs zenity 
+sudo apt-get install ssh fuse3 sshfs zenity 
 
 
 sudo ./softlink_setup
